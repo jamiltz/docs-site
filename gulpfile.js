ad198d16dc69ffd4278d2a7196f3cd73808dd83e
James Nocentini <james.nocentini@gmail.com>
2018-10-17 16:14:09 +0200

    revert some changes

--- gulpfile.js
+++ gulpfile.js
@@ -4,8 +4,32 @@ const connect = require('gulp-connect');
 const generator = require('@antora/site-generator-default');
 const gulp = require('gulp');
 
-require('./tasks/docs');
-require('./tasks/tutorials')
+let args = [
+  "--playbook",
+  "local-antora-playbook.yml"
+];
+
+gulp.task('build', function (cb) {
+  generator(args, process.env)
+    .then(() => {
+      cb();
+    })
+});
+
+gulp.task('preview', ['build'], function () {
+  /**
+   * Remove the line gulp.src('README.adoc')
+   * This is placeholder code to follow the gulp-connect
+   * example. Could not make it work any other way.
+   */
+  gulp.src('README.adoc')
+    .pipe(connect.reload());
+})
+
+gulp.task('watch', function () {
+  let dirs = ['../**/**.yml', '../**/**.adoc'];
+  gulp.watch(dirs, ['preview']);
+});
 
 gulp.task('connect', function() {
   connect.server({
@@ -16,6 +40,4 @@ gulp.task('connect', function() {
   });
 });
 
-gulp.task('docs', ['connect', 'docs:watch', 'docs:build'])
-gulp.task('tutorials', ['connect', 'tutorials:watch', 'tutorials:build'])
-gulp.task('default', ['docs', 'tutorials'])
+gulp.task('default', ['connect', 'watch', 'build'])
